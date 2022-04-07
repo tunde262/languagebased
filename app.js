aaf1becef3975e39a2e1da91d7a636a70ba55c4d
aivy23 <70339060+aivy23@users.noreply.github.com>
2022-04-07 16:52:27 -0500

    Added Aidan's code

--- app.js
+++ app.js
@@ -1,25 +1,128 @@
-// Import express to create server
-const express = require('express');
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var port = process.env.PORT || 3000;
+var serv = require('http').Server(app);
+const mongoose = require('mongoose');
 
-// DB connection file
-const connectDB = require('./config/db');
+require('dotenv').config();
 
-// auth routes file
-const auth = require('./routes/authRoutes');
+app.use(cors());
+app.use(express.json());
 
-// Initialize express server
-const app = express();
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
 
-connectDB();
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established");
+});
 
-// Body parser middleware
-app.use(express.json({ extended: false }));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-// Server endpoints (i.e. localhost:5000/api/auth)
-app.use('/api/auth', auth);
+serv.listen(port, () => {
+    console.log('Server started');
+});
 
-// Server Port #
-const port = 5000;
+var SOCKET_LIST = {}
+var PLAYER_LIST = {}
 
-// Launch|listen to Server
-app.listen(port, () => console.log(`Server running on port ${port}`));
+var Player = function(id) {
+    var self = {
+        x: 250,
+        y: 250,
+        id: id,
+        number: "" + Math.floor(10 * Math.random())
+    }
+    return self;
+}
+
+var USERS = {
+    "bob":"asd",
+    "bob2":"bob",
+    "bob3":"ttt",
+}
+
+var isValidPassword = function(data,cb) {
+    setTimeout(function() {
+        cb(USERS[data.username] === data.password);
+    },10);
+}
+
+var isUsernameTaken = function(data,cb) {
+    setTimeout(function() {
+        cb(USERS[data.username]);
+    },10);
+}
+
+var addUser = function(data,cb) {
+    setTimeout(function() {
+        USERS[data.username] = data.password;
+        cb();
+    },10);
+}
+
+var io = require('socket.io')(serv,{});
+io.on('connection', function(socket){
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+    
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
+    // console.log('Socket connection');
+
+    socket.on('signIn', function(data) {
+        isValidPassword(data,function(res){
+            if(res) {
+                //Player.onConnect(socket);
+                socket.emit('signInResponse', {success:true});
+            }
+            else
+                socket.emit('signInResponse', {success:false});
+        });
+    });
+
+    socket.on('signUp', function(data) {
+        isUsernameTaken(data, function(res){
+            if(res) {
+                //Player.onConnect(socket);
+                socket.emit('signUpResponse', {success:false});
+            }
+            else {
+                addUser(data, function(){
+                    socket.emit('signUpResponse', {success:true});
+                });
+            }
+        });
+    });
+
+
+    socket.on('disconnect', function() {
+        delete SOCKET_LIST[socket.id]; 
+        delete PLAYER_LIST[socket.id];
+    });
+});
+
+setInterval (function(){
+    var pack = [];
+    for(var i in PLAYER_LIST) {
+        var player = PLAYER_LIST[i];
+        player.x++;
+        player.y++;
+        pack.push({
+            x: player.x,
+            y: player.y,
+            number: player.number
+        })
+    }
+    for(var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPosition', pack); 
+
+    }
+}, 10000/25);
+
+
+
