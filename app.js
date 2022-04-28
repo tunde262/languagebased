779a48d22b09c58898f35c6194c80c473b9708f7
Tunde Adepitan <tundeadepitan@Tundes-MacBook-Air.local>
2022-04-28 18:10:56 -0500

    fixed file name issue with starting server

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 // DB connection file
 const connectDB = require('./config/db');
 
-// auth routes file
+// routes file
 const auth = require('./routes/authRoutes');
+const game = require('./routes/authRoutes');
 
 // Initialize express server
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json({ extended: false }));
 
 // Server endpoints (i.e. localhost:5000/api/auth)
 app.use('/api/auth', auth);
+app.use('/api/game', game);
 
 // Server Port #
 const port = 5000;
